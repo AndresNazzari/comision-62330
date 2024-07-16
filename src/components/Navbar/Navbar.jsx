a09fdaaba03f0f2d5ce185feaa2b828a1fbfeaa4
Andres J. Nazzari <andresnazzari@gmail.com>
2024-07-16 19:25:57 -0300

    cambios en el navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,15 +9,8 @@ const Navbar = (props) => {
     <>
       <div>
         <h1>{props.title}</h1>
-      </div>
-      <nav className="navItems">
-        <NavbarLink category={props.categories[0]} />
-        <NavbarLink category={props.categories[1]} />
-        <NavbarLink category={props.categories[2]} />
-        <NavbarLink category={props.categories[3]} />
-        <NavbarLink category={props.categories[4]} />
-      </nav>
-      <ItemCount stock={55} initial={55} />
+   
+     
       <CartWidget />
     </>
   );
